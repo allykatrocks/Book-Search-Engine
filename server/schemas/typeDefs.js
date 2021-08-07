a7276873eb5174b4aaa0ffd9d38a865d9e207f24
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-08-07 10:03:43 -0700

    installation, schemas, beginning functionality

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,18 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        
+    }
+`
+
+module.exports = typeDefs;
